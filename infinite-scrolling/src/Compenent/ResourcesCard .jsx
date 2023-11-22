607721fd9c66f60ad1c36a06c62cfe88b2c8bf08
Woranut Dangworawith <146110465+danworanut@users.noreply.github.com>
2023-11-22 16:42:34 +0700

    update decorate style

--- infinite-scrolling/src/Compenent/ResourcesCard .jsx
+++ infinite-scrolling/src/Compenent/ResourcesCard .jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Grid, Card as MaterialUICard, CardContent, Typography } from '@mui/material';
+import { Button } from '@mui/material';
 
 const ResourcesCard = () => {
   const [codingResources, setCodingResources] = useState([]);
@@ -11,7 +12,7 @@ const ResourcesCard = () => {
     try {
       const response = await axios.get('https://api.sampleapis.com/codingresources/codingResources');
 
-      if (response.data.length > 0) {
+      if (response.data.length > 4) {
         setCodingResources([...codingResources, ...response.data]);
       } else {
         setHasMore(false);
@@ -26,34 +27,38 @@ const ResourcesCard = () => {
   }, []); 
   
   return (
-    <div>
-      <h1>Coding Resources</h1>
+    <div style={{ padding: '20px'}}>
+      <h1 style={{ padding: '20px', textAlign: 'center', fontFamily: 'Poppins, sans-serif' }}>Coding Resources</h1>
       <InfiniteScroll
         dataLength={codingResources.length}
         next={fetchData}
         hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
+        loader={<Typography  variant="h5" color="#00000" style={{ ontWeight: 'bold',textAlign: 'center', margin: '50px 0',fontFamily: 'Poppins, sans-serif' }}>Loading...</Typography>}
       >
-        <Grid container spacing={3}>
+        <Grid container spacing={5} >
           {codingResources.map((resource, index) => (
-            <Grid item xs={12} sm={6} md={3} key={index}>
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index} style={{ margin: '16px 0' }}>
               <MaterialUICard style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <CardContent style={{ flex: 1 }}>
-                  <Typography variant="h6" component="div">
-                    {resource.description}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    URL: {resource.url}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Types: {resource.types.join(', ')}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Topics: {resource.topics.join(', ')}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Levels: {resource.levels.join(', ')}
-                  </Typography>
+                <CardContent style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' , fontFamily: 'Poppins, sans-serif' }}>
+                  <div>
+                  <Typography variant="body2" color="#C06C84" style={{ margin: '9px 0',fontSize: '12.5pt' , fontWeight: 'bold', fontFamily: 'Poppins, sans-serif' }}>
+                      {resource.topics.join(', ')}
+                    </Typography>
+                    <Typography variant="h5"  component="div" style={{ fontWeight: 'bold' ,fontFamily: 'Poppins, sans-serif'  }}>
+                      {resource.description}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" style={{ marginTop: '8px', fontFamily: 'Poppins, sans-serif' }}>
+                      Types: {resource.types.join(', ')}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" style={{ fontFamily: 'Poppins, sans-serif' }}>
+                      Levels: {resource.levels.join(', ')}
+                    </Typography>
+                  </div>
+                  <div style={{ textAlign: 'center', marginTop: '16px' }}>
+                    <Button variant="contained" color="primary"  style={{ fontFamily: 'Poppins, sans-serif'}}  href={resource.url} target="_blank">
+                      Visit Website
+                    </Button>
+                  </div>
                 </CardContent>
               </MaterialUICard>
             </Grid>
